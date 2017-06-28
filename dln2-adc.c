6aa21b4f82bf8828553734b5e61e3b51a00f0f8d
Jack Andersen <jackoalan@gmail.com>
2017-06-27 14:15:31 -1000

    4.11 fix

--- dln2-adc.c
+++ dln2-adc.c
@@ -515,7 +515,7 @@ static irqreturn_t dln2_adc_trigger_h(int irq, void *p)
 	}
 
 	iio_push_to_buffers_with_timestamp(indio_dev, &data,
-					   iio_get_time_ns());
+					   iio_get_time_ns(indio_dev));
 
 done:
 	iio_trigger_notify_done(indio_dev->trig);
